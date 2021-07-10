97c777f1481d072902a42c2867f2b984b5f62278
John Schmitt <marmalodak@gmail.com>
2021-07-09 23:47:35 -0700

    Instead of creating the venv in situ, write three files: 1. The requirements file 2. The script to create the venv and install the requirements 3. The activate script to activate the venv

--- bin/mk-venv.zsh
+++ bin/mk-venv.zsh
@@ -1,45 +1,68 @@
 #!/usr/bin/env zsh
 
+set -o pipefail
 set -e
 set -u
-set -o pipefail
 set +x
-
-# TODO
-# . write out the zsh script that creates the venv
 
 PARAMETERS=("${(@s/ /)*}")  # convert string to array https://stackoverflow.com/a/2930519/1698426
 VIRTUAL_ENV=${1:A}  # expand VIRTUAL_ENV to be the full path to the first parameter
+VIRTUAL_ENV_NAME=$(basename "${VIRTUAL_ENV}")
 shift PARAMETERS
 
-unset PYTHONHOME
-
-# create a new venv
-python3 -m venv ${VIRTUAL_ENV}
-
-# activate the new venv
-export VIRTUAL_ENV=${VIRTUAL_ENV}
-export PS1="($(basename \"$VIRTUAL_ENV\")${PS1}"
-export PATH=${VIRTUAL_ENV}/bin:"$PATH"
-
-# upgrade pip and install required packages
-pip install --upgrade pip  # avoid the pip prompt to upgrade
 if [[ ${#PARAMETERS[@]} -ne 0 ]]; then
-    pip --no-input install ${PARAMETERS}
+    REQUIREMENTS_FILE="${1}"-requirement.text
 fi
 
+# ---- create the activate script
 activate_doc=$(cat << 'activate_doc_end'
 #!/usr/bin/env zsh
 
+set -o pipefail
+set -e
+set -u
+set +x
+
 unset PYTHONHOME
 export VIRTUAL_ENV=VIRTUAL_ENV_PLACE_HOLDER
-export PS1=$(basename "${VIRTUAL_ENV}) ${PS1}"
+export PS1=$(basename "${VIRTUAL_ENV}" "${PS1}")
 export PATH="${VIRTUAL_ENV}/bin:${PATH}"
 
 zsh -i
 activate_doc_end
 )
 
+activate_script_name="${VIRTUAL_ENV}-activate.zsh"
 activate_script="${activate_doc/VIRTUAL_ENV_PLACE_HOLDER/${VIRTUAL_ENV}}"
-echo "${activate_script}" > "${VIRTUAL_ENV}-activate.zsh"
-chmod +x "${VIRTUAL_ENV}-activate.zsh"
+echo "${activate_script}" > "${activate_script_name}"
+chmod +x "${activate_script_name}"
+
+# ---- create the script to create the virtualenv and alsoe the requirements file
+mkvenv_doc=$(cat << 'mkvenv_doc_end'
+#!/usr/bin/env zsh
+
+set -o pipefail
+set -e
+set -u
+set +x
+
+python3 -m venv VIRTUAL_ENV_PLACE_HOLDER
+VIRTUAL_ENV_PLACE_HOLDER/bin/pip3 install --upgrade pip
+REQUIREMENTS_PLACE_HOLDER
+mkvenv_doc_end
+)
+
+mkvenv_script_name="mk-${VIRTUAL_ENV_NAME}-venv.zsh"
+mkvenv_doc="${mkvenv_doc/VIRTUAL_ENV_PLACE_HOLDER/${VIRTUAL_ENV}}"
+mkvenv_doc="${mkvenv_doc/VIRTUAL_ENV_PLACE_HOLDER/${VIRTUAL_ENV}}"  # yes, this line is duplicated because VIRTUAL_ENV_PLACE_HOLDER occurs twice
+if [[ -v REQUIREMENTS_FILE ]]; then
+    echo "#  requirements for ${VIRTUAL_ENV_NAME}" > "${REQUIREMENTS_FILE}"
+    for parameter in ${PARAMETERS}; do
+        echo "${parameter}" >> "${REQUIREMENTS_FILE}"
+    done
+    mkvenv_script="${mkvenv_doc/REQUIREMENTS_PLACE_HOLDER/${VIRTUAL_ENV}/bin/pip3 install --requirement ${REQUIREMENTS_FILE}}"
+else
+    mkvenv_script="${mkvenv_doc/REQUIREMENTS_PLACE_HOLDER/}"
+fi
+echo "${mkvenv_script}" > "${mkvenv_script_name}"
+chmod +x "${mkvenv_script_name}"
