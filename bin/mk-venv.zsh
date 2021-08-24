e97cbadf0a5715e3d8d2149d4640e1364fba1bd2
John Schmitt <marmalodak@gmail.com>
2021-08-24 16:48:30 -0700

    mk-venv appends -venv to the name of the venv

--- bin/mk-venv.zsh
+++ bin/mk-venv.zsh
@@ -5,12 +5,12 @@ set -e
 set -u
 set +x
 
-# TODO: write a script like $venv-add pipname that will maintain the requirements.txt file
-# does this need a $venv-remove
+# should there be automation for adding or removing packages from the requirements file?
+# or should the customer simply modify the requirements file by hand?
 
 PARAMETERS=("${(@s/ /)*}")  # convert string to array https://stackoverflow.com/a/2930519/1698426
 VIRTUAL_ENV=${1:A}  # expand VIRTUAL_ENV to be the full path to the first parameter
-VIRTUAL_ENV_NAME=$(basename "${VIRTUAL_ENV}")
+VIRTUAL_ENV_NAME=$(basename "${VIRTUAL_ENV}")-venv
 shift PARAMETERS
 
 if [[ ${#PARAMETERS[@]} -ne 0 ]]; then
@@ -35,13 +35,12 @@ zsh -i
 activate_doc_end
 )
 
-activate_script_name="${VIRTUAL_ENV}-activate.zsh"
-activate_script="${activate_doc/VIRTUAL_ENV_PLACE_HOLDER/${VIRTUAL_ENV}}"
+activate_script_name="${VIRTUAL_ENV_NAME}-activate.zsh"
+activate_script="${activate_doc/VIRTUAL_ENV_PLACE_HOLDER/${VIRTUAL_ENV_NAME}}"
 echo "${activate_script}" > "${activate_script_name}"
 chmod +x "${activate_script_name}"
 
-# TODO: the venv should have -venv appended to it
-# ---- create the script to create the virtualenv and alsoe the requirements file
+# ---- create the script to create the virtualenv and also the requirements file
 mkvenv_doc=$(cat << 'mkvenv_doc_end'
 #!/usr/bin/env zsh
 
@@ -51,20 +50,19 @@ set -u
 set +x
 
 python3 -m venv VIRTUAL_ENV_PLACE_HOLDER
-VIRTUAL_ENV_PLACE_HOLDER/bin/pip3 install --upgrade pip
+VIRTUAL_ENV_PLACE_HOLDER/bin/pip3 --no-input install --upgrade pip
 REQUIREMENTS_PLACE_HOLDER
 mkvenv_doc_end
 )
 
-mkvenv_script_name="mk-${VIRTUAL_ENV_NAME}-venv.zsh"
-mkvenv_doc="${mkvenv_doc/VIRTUAL_ENV_PLACE_HOLDER/${VIRTUAL_ENV}}"
-mkvenv_doc="${mkvenv_doc/VIRTUAL_ENV_PLACE_HOLDER/${VIRTUAL_ENV}}"  # yes, this line is duplicated because VIRTUAL_ENV_PLACE_HOLDER occurs twice
+mkvenv_script_name="mk-${VIRTUAL_ENV_NAME}.zsh"
+mkvenv_doc="${mkvenv_doc//VIRTUAL_ENV_PLACE_HOLDER/${VIRTUAL_ENV_NAME}}"
 if [[ -v REQUIREMENTS_FILE ]]; then
     echo "#  requirements for ${VIRTUAL_ENV_NAME}" > "${REQUIREMENTS_FILE}"
     for parameter in ${PARAMETERS}; do
         echo "${parameter}" >> "${REQUIREMENTS_FILE}"
     done
-    mkvenv_script="${mkvenv_doc/REQUIREMENTS_PLACE_HOLDER/${VIRTUAL_ENV}/bin/pip3 install --requirement ${REQUIREMENTS_FILE}}"
+    mkvenv_script="${mkvenv_doc/REQUIREMENTS_PLACE_HOLDER/${VIRTUAL_ENV_NAME}/bin/pip3 --no-input install --requirement ${REQUIREMENTS_FILE}}"
 else
     mkvenv_script="${mkvenv_doc/REQUIREMENTS_PLACE_HOLDER/}"
 fi
